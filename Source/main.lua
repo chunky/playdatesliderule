908422d4e8863706f1e35bfdd70d1ca21c17a7a1
Gary Briggs <gbriggs@rand.org>
2023-09-07 12:22:13 -0700

    Hoist DrawTick function

--- Source/main.lua
+++ Source/main.lua
@@ -21,9 +21,11 @@ local gfx <const> = playdate.graphics
 -- Global variable pile-on!
 local default_n_powers_to_display = 4
 local default_base = 10
+local axis_options = { "log", "linear" }
 
 local n_powers_to_display = default_n_powers_to_display
 local base = default_base
+local current_axis = 1
 
 
 -- A function to set up our game environment.
@@ -46,7 +48,7 @@ function myGameSetUp()
         end,
 
         leftButtonDown = function()
-            base = math.min(base+1, 10)
+            base = math.min(base+1, 16)
         end,
 
         rightButtonDown = function()
@@ -56,6 +58,13 @@ function myGameSetUp()
         BButtonDown = function()
             n_powers_to_display = default_n_powers_to_display
             base = default_base
+        end,
+
+        AButtonDown = function()
+            current_axis += 1
+            if current_axis > #axis_options then
+                current_axis = 1
+            end
         end
 
     }
@@ -78,6 +87,32 @@ DEG_TO_RAD = 0.01745329
 
 function playdate.update()
 
+    local function drawTick(angle_deg, circle_r, circle_center, label, label_radius_fudge)
+        local crankangle = playdate.getCrankPosition()
+        local theta_deg = angle_deg + crankangle
+        while(theta_deg > 360) do
+            theta_deg = theta_deg - 360
+        end
+        
+        local v1 = playdate.geometry.vector2D.newPolar(circle_r-10, theta_deg)
+        local v2 = playdate.geometry.vector2D.newPolar(circle_r+5, theta_deg)
+
+        local start_p = circle_center + v1
+        local end_p = circle_center + v2
+        gfx.drawLine(start_p.x, start_p.y, end_p.x, end_p.y)
+
+        if label and string.len(label) > 0 then
+            local font_v = playdate.geometry.vector2D.newPolar(label_radius_fudge, theta_deg)
+
+            local text_width, text_height = gfx.getTextSize(label)
+            
+            local font_height_fudge = math.sin(theta_deg * DEG_TO_RAD / 2.0) * text_height
+            local font_width_fudge = math.sin(theta_deg * DEG_TO_RAD) * text_width
+
+            gfx.drawTextAligned(label, start_p.x - font_width_fudge + font_v.x, start_p.y - font_height_fudge + font_v.y, kTextAlignment.center)
+        end
+    end
+
     playdate.graphics.clear()
 
     local h = playdate.display.getHeight()
@@ -92,33 +127,16 @@ function playdate.update()
 
     gfx.drawCircleAtPoint(center_p, circle_r)
     
-    local mode = "log"
+    local mode = axis_options[current_axis]
 
     if "linear" == mode then
         for i = 0.0, 359.0, 45.0 do
             local num_str = string.format("%.0f", i)
-            local crankangle = playdate.getCrankPosition()
-            local theta_deg = crankangle + i
-            while(theta_deg > 360) do
-                theta_deg = theta_deg - 360
-            end
-            local v1 = playdate.geometry.vector2D.newPolar(circle_r-10, theta_deg)
-            local v2 = playdate.geometry.vector2D.newPolar(circle_r+5, theta_deg)
-
-            local text_width, text_height = gfx.getTextSize(num_str)
-            
-            local font_height_fudge = math.sin(theta_deg * DEG_TO_RAD / 2.0) * text_height
-            local font_width_fudge = math.sin(theta_deg * DEG_TO_RAD) * text_width
-
-            local start_p = center_p + v1
-            local end_p = center_p + v2
-            gfx.drawLine(start_p.x, start_p.y, end_p.x, end_p.y)
-            gfx.drawTextAligned(num_str, start_p.x - font_width_fudge, start_p.y - font_height_fudge, kTextAlignment.center)
+            drawTick(i, circle_r, center_p, num_str, 0)
         end
     elseif "log" == mode then
         local total_angle_range = 360
         local last_indicator = base^n_powers_to_display
-        local crankangle = playdate.getCrankPosition()
         local done = false
         local next_val = 0
         local current_delta = 1
@@ -141,13 +159,9 @@ function playdate.update()
                 done = true
             end
 
-            local num_str = string.format("%i", ticks_this_power)
+            local num_str = string.format("%x", ticks_this_power)
 
-            local theta_deg = this_angle + crankangle
-            while(theta_deg > 360) do
-                theta_deg = theta_deg - 360
-            end
-
+            local label_radius_fudge = 0
             if show_full_value then
                 local n_zeros = math.floor(math.log(next_val, base))
                 num_str = string.format("%i", next_val)
@@ -155,23 +169,10 @@ function playdate.update()
                 for i = 1, n_zeros do
                     num_str = num_str .. "0"
                 end
-                font_v = playdate.geometry.vector2D.newPolar(-8, theta_deg)
+                label_radius_fudge = -8
             end
 
-            local v1 = playdate.geometry.vector2D.newPolar(circle_r-10, theta_deg)
-            local v2 = playdate.geometry.vector2D.newPolar(circle_r+5, theta_deg)
-            local font_v = playdate.geometry.vector2D.newPolar(0, 0, theta_deg)
-
-            local text_width, text_height = gfx.getTextSize(num_str)
-            
-            local font_height_fudge = math.sin(theta_deg * DEG_TO_RAD / 2.0) * text_height
-            local font_width_fudge = math.sin(theta_deg * DEG_TO_RAD) * text_width
-
-            local start_p = center_p + v1
-            local end_p = center_p + v2
-            gfx.drawLine(start_p.x, start_p.y, end_p.x, end_p.y)
-            gfx.drawTextAligned(num_str, start_p.x - font_width_fudge + font_v.x, start_p.y - font_height_fudge + font_v.y, kTextAlignment.center)
-
+            drawTick(this_angle, circle_r, center_p, num_str, label_radius_fudge)
         end
     end
     
